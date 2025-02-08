b79efa751b1cfe51086b0c13cfbf01bfedd90845
Liza De Guerengomba <LDeGuereng@my.gcu.edu>
2025-02-08 14:41:18 -0700

    firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
-import { getFirestore } from "firebase/firestore";
 
 
 // web app's Firebase configuration
